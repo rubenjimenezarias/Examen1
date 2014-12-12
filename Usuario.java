f8fd008ae2e81c096e8f72556242688146aba766
rubenjimenezarias <rubenjimenezarias@gmail.com>
2014-12-12 13:09:45 +0100

    Commit 3 y 4: realizado metodo para comparar y modificado metodo para ver el porcentaje de los alimentos ingeridos

--- Usuario.java
+++ Usuario.java
@@ -31,7 +31,7 @@ public class Usuario
     /**
      * Sumamos lo ingerido al usuario
      */
-    public void comer(Alimento comida, int gramos)
+    public void comer(Alimento comida, float gramos)
     {
         // cogemos el alimento 
         proteina = proteina + ((comida.proteina()/100)*gramos);
@@ -45,11 +45,61 @@ public class Usuario
      */
     public void estadoComidaIngerida()
     {
+        //calculamos los porcentajes de los alimentos
+        float total = (proteina + carbo + grasa);
+        float porproteina = (proteina*100)/total;
+        float porcarbo = (carbo*100)/total;
+        float porgrasa = (grasa*100)/total;
+        
         // mostramos los datos por pantalla el estado del estomago del usuario
         System.out.println("Nombre: " + nomusu);
-        System.out.println("Proteina: " + proteina + " gramos.");
-        System.out.println("Carbohidratos: " + carbo + " gramos.");
-        System.out.println("Grasa: " + grasa + " gramos.");
+        if (porproteina <= 0){
+            System.out.println("Proteina: " + proteina + " gramos.");
+        }
+        else {
+            System.out.println("Proteina: " + proteina + " gramos.   " + porproteina + "%.");
+        }
+        if (porcarbo <= 0){
+            System.out.println("Proteina: " + carbo + " gramos.");
+        }
+        else {
+            System.out.println("Proteina: " + carbo + " gramos.   " + porcarbo + "%.");
+        }
+        if (porgrasa <= 0){
+            System.out.println("Proteina: " + grasa + " gramos.");
+        }
+        else {
+            System.out.println("Proteina: " + grasa + " gramos.   " + porgrasa + "%.");
+        }
         System.out.println("Calorias: " + caloria + " gramos.");
+    }
+    /**
+     * Devuelve el nombre del Usuario
+     */
+    public String nombre()
+    {
+        return nomusu;
+    }
+    /**
+     * Devuelve las calorias del usuario
+     */
+    public float caloria()
+    {
+        return caloria;
+    }
+    /**
+     * Comparamos entre dos usuarios quien ha comido mas calorias y lo mostramos por pantalla
+     */
+    public void compararCalorias(Usuario compararCon)
+    {
+        if (caloria > compararCon.caloria()){
+            System.out.println(nomusu + " ha comido mas calorias que " + compararCon.nombre());
+        }
+        else if (caloria < compararCon.caloria()){
+            System.out.println(compararCon.nombre() + " ha comido mas calorias que " + nomusu);
+        }
+        else {
+            System.out.println("Los dos han comido la misma cantidad de calorias.");
+        }
     }
 }
